f06c46d139ceeeee5dc6b85a4d59a46f43708bd8
Natalia Gubanova <khomtali@gmail.com>
2020-06-03 11:09:42 +0200

    add component key

--- src/components/calculator/ZoneRow.js
+++ src/components/calculator/ZoneRow.js
@@ -3,18 +3,17 @@ import PropTypes from 'prop-types';
 
 function ZoneRow(props) {
   return (
-    <tr key={props.index}>
-      <th key={props.index + '1'}>{props.range.join(' - ')}</th>
-      <th key={props.index + '2'}>{props.description.effort}</th>
-      <th key={props.index + '3'}>{props.description.effect}</th>
+    <tr>
+      <th>{props.range.join(' - ')}</th>
+      <th>{props.description.effort}</th>
+      <th>{props.description.effect}</th>
     </tr>
   )
 }
 
 ZoneRow.propTypes = {
   range: PropTypes.array.isRequired,
-  description: PropTypes.object.isRequired,
-  index: PropTypes.number.isRequired
+  description: PropTypes.object.isRequired
 };
 
 export default ZoneRow;
