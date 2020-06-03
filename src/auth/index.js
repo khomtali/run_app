ea1365698fb93faee0211fb572cbb0686b8125ee
Travis Shears <btbtravis@gmail.com>
2020-06-03 20:49:39 +0200

    init new signup button

---
+++ src/auth/index.js
@@ -0,0 +1,58 @@
+let auth0;
+
+const backendURL = process.env.REACT_APP_BACKEND_URL;
+
+export const configureClient = async () => {
+    if (typeof auth0 !== 'undefined') {
+        console.log('skipping config')
+        return;
+    }
+    const response = await fetch(backendURL + '/client-info');
+    const config = await response.json();
+
+    // createAuth0Client comes from auth0 script tag
+    auth0 = await window.createAuth0Client({
+        domain: config.domain,
+        client_id: config.client_id
+    });
+};
+
+export const handleAuth0Callback = async () => {
+    const query = window.location.search;
+    if (query.includes("code=") && query.includes("state=")) {
+        await auth0.handleRedirectCallback();
+        window.history.replaceState({}, document.title, "/");
+    }
+};
+
+export const getUser = async () => {
+    if (typeof auth0 === "undefined" || !(await isAuthenticated())) {
+        return;
+    }
+
+    return {
+        token: await auth0.getTokenSilently(),
+        remote_data: await auth0.getUser()
+    }
+};
+
+export const login = () => auth0.loginWithRedirect({
+    redirect_uri: window.location.origin
+});
+
+
+export const isAuthenticated = async () => {
+    return await auth0.isAuthenticated();
+};
+
+export const logout = () => auth0.logout({
+    returnTo: window.location.origin
+});
+
+export const authFlow = async (cb) => {
+    await configureClient();
+    await handleAuth0Callback();
+    if (await isAuthenticated()) {
+        cb(await getUser());
+    }
+}
---
+++ src/components/Signup.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { login, logout, authFlow } from '../auth';
+
+function Signup() {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    authFlow((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  const handleSignupClick = async () => {
+    login();
+  }
+
+  const handleLogoutClick = async () => {
+    logout();
+  }
+  return (
+    <div>
+      {!user ?
+        <button type="button" onClick={handleSignupClick}>Signup/Login</button>
+        :
+        <button type="button" onClick={handleLogoutClick}>Logout</button>
+      }
+      <p>Username: {user ? user.remote_data.nickname : '?'}</p>
+      <p>Token: {user && user.token ? user.token : '?'}</p>
+    </div>
+  );
+}
+
+export default Signup;
