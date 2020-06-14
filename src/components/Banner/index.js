7139427792c5bd51e0935e5fdd829e2754fbedff
Natalia Gubanova <nataliagubanova@MacBook-Pro.home>
2020-06-15 01:14:39 +0200

    add page banner

---
+++ src/components/Banner/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+function Banner() {
+  return (
+    <div className="banner-container">
+      <span className="banner-container__author"><i>photo by
+        <a href="https://www.instagram.com/iamzykova.ph/" target="_blank" rel="noopener noreferrer"> @iamzykova.ph</a>
+      </i></span>
+    </div>
+  );
+}
+
+export default Banner;
