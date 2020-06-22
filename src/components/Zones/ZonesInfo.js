b8cfba44e33879b6ef43383a6bb397746d024b86
Natalia Gubanova <nataliagubanova@MacBook-Pro.home>
2020-06-23 01:23:26 +0200

    add ZonesInfo cmp

---
+++ src/components/Zones/ZonesInfo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ZonesInfo() {
+  return (
+    <div className="content__zones__info">
+      <p>
+        Heart rate is useful in because it helps athletes avoid the single most common training mistake:
+        pushing too hard in workouts that are supposed to be done at low intensity (Zones 1 and 2).
+      </p>
+    </div>
+  );
+}
+
+export default ZonesInfo;
